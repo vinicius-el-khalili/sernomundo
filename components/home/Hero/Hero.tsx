ff82f7cfa53318d9aee7b6b33454f236bbed6171
vini <velkhaliliborsato@gmail.com>
2023-12-16 12:11:21 -0300

    letsgo

--- components/home/Hero/Hero.tsx
+++ components/home/Hero/Hero.tsx
@@ -1,26 +1,49 @@
+"use client"
+
 import Image from "next/image";
 import style from "./Hero.module.scss"
 import ZapIcon from "@/components/ZapButton/ZapIcon";
+import gsap from "gsap";
+import { useLayoutEffect, useRef } from "react";
 
 const Hero = () => {
+
+    const rootRef = useRef<HTMLDivElement|null>(null)
+    const timelineRef = useRef<gsap.core.Timeline|null>(null)
+
+    useLayoutEffect(()=>{
+        const context = gsap.context(()=>{
+            timelineRef.current = gsap.timeline()
+            .fromTo("._",{},{})
+            .fromTo(".heroImage",{x:-10,opacity:0},{x:0,opacity:1})
+            .fromTo(".heroHeader",{x:-20,opacity:0},{x:-20,opacity:1})
+            .fromTo(".heroSubText",{y:-10,color:"transparent"},{y:0,color:"black",transition:2})
+            .fromTo(".zapButton",{y:-10,opacity:0},{y:0,opacity:1,transition:2})
+        }, rootRef)
+    },[])
     return (
-        <div className={style.Hero}>
-            <div className={style.heroImage}>
+        <div className={style.Hero} ref={rootRef}>
+            <div className={`${style.heroImage} heroImage`}>
                 <Image src={"/logo01.jpeg"} fill style={{objectFit:"cover"}} alt=""/>
             </div>
-            <div className={style.heroHeader}>
+            <div className={`${style.heroHeader} heroHeader`}>
                 <h1>Ser no mundo</h1>
             </div>
-            <div className={style.heroSubText}>
+            <div className={`${style.heroSubText} heroSubText`}>
                 <p>
                 Entre fronteiras, descubra-se. Explore a terapia como sua bússola pessoal para adaptar-se, prosperar e florescer em terras estrangeiras.
                 </p>
             </div>
-            <div className={style.zapButton}>
+            <div className={`${style.zapButton} zapButton`}>
                 <ZapIcon/> Agendar consulta
             </div>
+            <Ok/>
         </div>
     );
 }
  
 export default Hero;
+
+const Ok = () => {
+    return <><button onClick={()=>{console.log("ok")}}></button></>
+}
--- components/layout/Navbar/Navbar.tsx
+++ components/layout/Navbar/Navbar.tsx
@@ -2,12 +2,18 @@
 import { useState } from "react";
 import NavButton from "./NavButton";
 import style from "./Navbar.module.scss"
+import Link from "next/link";
 const Navbar = () => {
     const [menu,setMenu] = useState<boolean>(false)
     const toggleMenu = ()=>{setMenu(!menu)}
     return (
         <nav className={!menu?style.Navbar:`${style.Navbar} ${style.NavbarOn}`}>
             <NavButton menu={menu} toggleMenu={toggleMenu}/>
+            <Link className={style.link} href={"/"} onClick={toggleMenu}><p>Home</p></Link>
+            <Link className={style.link} href={"/"} onClick={toggleMenu}><p>Sobre</p></Link>
+            <Link className={style.link} href={"/"} onClick={toggleMenu}><p>Serviços</p></Link>
+            <Link className={style.link} href={"/"} onClick={toggleMenu}><p>Artigos</p></Link>
+            <Link className={style.link} href={"/"} onClick={toggleMenu}><p>Contato</p></Link>
         </nav>
     );
 }
